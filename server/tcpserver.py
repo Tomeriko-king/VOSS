d34a6420850789420648e87ae25ee3f5705d3208
Tomeriko-king <tomerklein9@gmail.com>
2024-12-13 13:00:41 +0200

    Support handling multiple connections by handle asynchronously

--- server/tcpserver.py
+++ server/tcpserver.py
@@ -1,27 +1,32 @@
 import socket
+from threading import Thread
+
 from ftpserver import start_ftp_server
-from ftplib import FTP
-from PIL import ImageGrab
+
+HOST = '127.0.0.1'  # Localhost
+PORT = 12345  # Arbitrary port for the server
+
 
 def start_server():
-    # Server configuration
-    host = '127.0.0.1'  # Localhost
-    port = 12345         # Arbitrary port for the server
-
     # Create a socket object
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Bind the socket to the server address and port
-    server_socket.bind((host, port))
+    server_socket.bind((HOST, PORT))
 
     # Enable the server to accept connections (max 5 clients in the waiting queue)
     server_socket.listen(5)
-    print(f"Server started on {host}:{port} and waiting for connections...")
+    print(f"Server started on {HOST}:{PORT} and waiting for connections...")
 
-    # Accept a connection from the client
-    client_socket, client_address = server_socket.accept()
-    print(f"Connected to {client_address}")
+    while True:
+        # Accept a connection from the client
+        client_socket, client_address = server_socket.accept()
+        print(f"Connected to {client_address}")
+        handle_client_thread = Thread(target=handle_connection, args=[client_socket])
+        handle_client_thread.start()
 
+
+def handle_connection(client_socket: socket.socket):
     # Receive data from the client
     message = client_socket.recv(1024).decode('utf-8')
     print(f"Received from client: {message}")
