31dfbb59402f424e4ea3095a8e2273ed0de6fe96
Tomeriko-king <tomerklein9@gmail.com>
2024-12-20 12:01:56 +0200

    Add support in image in email and remove poc code

--- client/email_sender.py
+++ client/email_sender.py
@@ -1,6 +1,10 @@
 import smtplib
-from datetime import datetime
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from email.mime.image import MIMEImage
 
 
 class EmailSender:
@@ -8,31 +12,19 @@ class EmailSender:
         self.sender = sender
         self.password = password
 
-    def send_email(self, subject: str, body: str, recipients: list[str]) -> None:
+    def send_email(self, subject: str, body: str, image_path: str, recipients: list[str]) -> None:
         msg = MIMEText(body)
         msg['Subject'] = subject
         msg['From'] = self.sender
         msg['To'] = ', '.join(recipients)
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
             smtp_server.login(self.sender, self.password)
+            # msg.attach(MIMEText(body, 'plain'))
+            # with open(image_path, 'rb') as img_file:
+            #     img = MIMEImage(img_file.read())
+            #     img.add_header('Content-ID', '<image1>')  # Optional: Use this if you want to display inline
+            #     msg.attach(img)
             smtp_server.sendmail(self.sender, recipients, msg.as_string())
 
 
-current_datetime = datetime.now()
-formatted_date = current_datetime.strftime("%Y-%m-%d")
-formatted_time = current_datetime.strftime("%H:%M:%S")
 
-# Your email credentials
-sender_email = 'tomerklein9@gmail.com'
-sender_password = ''  # TODO
-
-# Receiver's email address
-receiver_email = 'alonmergi2020@gmail.com'
-
-king = EmailSender(sender_email, sender_password)
-
-# Create the email content
-subject = 'Hacking attempt alert'
-body = f'Someone tried to hack in the app and failed to log in after 5 attempts. \n Date: {formatted_date} \n Time: {formatted_time} '
-
-king.send_email(subject, body, [receiver_email])
